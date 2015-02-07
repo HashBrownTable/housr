f2ece2967ec28206a560b5760bdc851b46e438c6
jennylia <jennylian100@gmail.com>
2015-02-07 12:12:45 -0800

    This is how you would start interacting with the model

--- client/app/pref/pref.controller.js
+++ client/app/pref/pref.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('housrApp')
-  .controller('PrefCtrl', function ($scope) {
+  .controller('PrefCtrl', function ($scope, User) {
     $scope.message = 'Hello';
+    $scope.save = function() {
+    	alert('Hello, ' + $scope.user.firstName);
+    }
+    $scope.user = {
+    	firstName: 'Bob!'
+    }
+    User.get(function(data) {
+    	debugger;
+    });
+
+
   });
