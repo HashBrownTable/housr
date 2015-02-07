5c40a63c944fec75b7e8e445e246be9a5aaab31d
jennylia <jennylian100@gmail.com>
2015-02-07 12:21:15 -0800

    Some more debugging items

--- client/app/pref/pref.controller.js
+++ client/app/pref/pref.controller.js
@@ -5,10 +5,14 @@ angular.module('housrApp')
     $scope.message = 'Hello';
     $scope.save = function() {
     	alert('Hello, ' + $scope.user.firstName);
+
     }
     $scope.user = {
-    	firstName: 'Bob!'
+    	firstName: $scope.user.firstName;
+
     }
+
+    //this is for debugging
     User.get(function(data) {
     	debugger;
     });
