376668ec58b42a8afedb9ac8a785942a956005b4
Tristan Rice <rice@fn.lc>
2015-02-07 22:44:44 -0800

    Minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# housr
+# ![Housr]()
+
+
+Housr (pronounced House-er) is a roommate finding service targeted towards anyone who would prefer to save some money by pooling money with a group of people.
--- client/app/details/details.controller.js
+++ client/app/details/details.controller.js
@@ -12,24 +12,25 @@ angular.module('housrApp')
 
     LikeDislike.get(function(data){
     		$scope.Likes = data;
-    		console.log("here comes the likes");
-    		console.log(data);
 
-    		data.forEach(function(Likes,i){
+    		_.filter(data, function(i) {
+          return i.type == 'like';
+        }).forEach(function(Likes,i){
     			User.get({id: Likes.targetId},function(x){
     				$scope.Likes[i].userData = x;
     				$scope.Likes[i].name = x.name;
     				$scope.Roomates.push(x.name);
+            $scope.Roomates = _.uniq($scope.Roomates);
     				//console.log(x.name);
     				//console.log("scopeLikes1");
     				//console.log($scope.Likes);
     			});
     		});
-    	
+
     	//console.log("scopeLikes2");
     	//console.log($scope.Roomates);
     });
-    
+
 
     User.get({id: $stateParams.id}, function(user) {
       $scope.user = user;
