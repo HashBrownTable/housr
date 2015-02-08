8b669a169d37ab9cc9445fd0daba44baeda56a27
unnatisapre <unnatisapre@gmail.com>
2015-02-07 21:48:39 -0800

    dislike template

---
+++ client/app/dislike/dislike.controller.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Controller: DislikeCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('housrApp'));
+
+  var DislikeCtrl, scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    DislikeCtrl = $controller('DislikeCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});
---
+++ client/app/dislike/dislike.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('housrApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('dislike', {
+        url: '/dislike',
+        templateUrl: 'app/dislike/dislike.html',
+        controller: 'DislikeCtrl'
+      });
+  });
