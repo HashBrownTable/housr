eb1e9fe0b5956d64745e924f30fa546dfb6a884e
unnatisapre <unnatisapre@gmail.com>
2015-02-07 16:39:31 -0800

    Adding google maps template

---
+++ client/app/maps/maps.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('housrApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('maps', {
+        url: '/landlords',
+        templateUrl: 'app/maps/maps.html',
+        controller: 'MapsCtrl'
+      });
+  });
