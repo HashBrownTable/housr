b1d2813e68c21ac608b5261f77e88105daed8f41
unnatisapre <unnatisapre@gmail.com>
2015-02-07 20:11:17 -0800

    fix

--- client/app/like/like.controller.js
+++ client/app/like/like.controller.js
@@ -1,16 +1,7 @@
 'use strict';
 angular.module('housrApp')
   .controller('LikeCtrl', function ($scope, LikeDislike) {
-    $scope.message = 'Hello';
-  
-  LikeDislike.get(function(likedislike))
-  
-  exports.index = function(req, res) {
-  Likedislike.find({ownerId: req.user._id}, function (err, likedislikes) {
-    if(err) { return handleError(res, err); }
-    return res.json(200, likedislikes);
-  });
-  
+    $scope.message = 'Hello';  
   });
   
   
