f0d1b340158d0453f3b341504d901c9388c27ac1
Vincent Chan <vincent@vincentcchan.com>
2015-02-07 20:03:07 -0800

    chat shows recipient name now

--- client/app/chats/chats.controller.js
+++ client/app/chats/chats.controller.js
@@ -1,10 +1,37 @@
 'use strict';
 
 angular.module('housrApp')
-  .controller('ChatsCtrl', function ($scope, Chats) {
+  .controller('ChatsCtrl', function ($scope, Chats, User) {
+
+    var currUserId = 'abc';
+    var recipientUserId = 'abc';
+
     $scope.message = 'Hello';
+
     Chats.get(function(data) {
       $scope.chats = data.chats;
       console.log($scope.chats);
+      console.log(data);
+
+
+      User.get(function(user){
+        currUserId = user._id;
+        console.log(currUserId);
+        $scope.chats.forEach(function(chat, i){
+          chat.people.forEach(function(personId){
+            if(personId!==currUserId){
+              recipientUserId = personId;
+              console.log(recipientUserId);
+              User.get({id: recipientUserId}, function(data){
+                console.log(data);
+                $scope.chats[i].names = data.name;
+                console.log($scope.chats);
+              });
+            }
+          });
+        });
+      });
+
     });
+
   });
